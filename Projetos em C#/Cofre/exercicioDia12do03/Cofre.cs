895bf66b1dcc082792c2467dd06c03349b98725f
Heitorlima65 <heitorlima797@gmail.com>
2026-06-29 09:06:54 -0300

    adicionei um exercicio
    adicionei o exercício do cofre

---
+++ Projetos em C#/Cofre/exercicioDia12do03/Cofre.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioDia12do03
+{
+    internal class Cofre
+    {
+        public double saldo;
+        public string senha = "1423";
+        public string estado;
+        public double saque;
+        public Cofre()
+        {
+            estado = "fechado";
+        }
+        public void abrir(string senha)
+        {
+            if(senha == this.senha)
+            {
+                Console.WriteLine("senha correta, Cofre aberto!");
+            }
+            else
+            {
+                Console.WriteLine("senha errada, Cofre echado!");
+            }
+            if(estado == "aberto")
+            {
+                Console.WriteLine("cofre ja aberto");
+                estado= "aberto";
+            }
+            else if(estado == "fechado")
+            {
+                Console.WriteLine("cofre aberto");
+                estado = "aberto";
+            }
+            Console.ReadKey();
+        }
+        public void fechar(string senha)
+        {
+            if (senha == this.senha)
+            {
+                if (estado == "aberto")
+                {
+                    Console.WriteLine("cofre fechado");
+                    estado = "fechado";
+                }
+                else if (estado == "fechado")
+                {
+                    Console.WriteLine("cofre ja fechado");
+                    estado = "fechado";
+                }
+            }
+            else
+            {
+                Console.WriteLine("senha errada!");
+            }
+            Console.ReadKey();
+        }
+        public void depositar(string senha)
+        {  
+            if(senha == this.senha)
+            {
+                Console.WriteLine("qual seria o valor a ser depositado?");
+                int depositar = int.Parse(Console.ReadLine());
+                saldo += depositar;
+                Console.WriteLine("seu saldo atual é de:\t" + saldo);
+            }
+            else
+            {
+                Console.WriteLine("não foi possivel abrir o cofre, senha errada!");
+            }
+            Console.ReadKey();
+        } 
+        public void sacar(string senha)
+        {
+            if (senha == this.senha)
+            {
+                Console.WriteLine("qual é o valor a ser sacado?");
+                saque = double.Parse(Console.ReadLine());
+                if((saque == saldo) || (saque < saldo))
+                {
+                    Console.WriteLine("você sacou o valor de:\t" + saque);
+                    saldo -= saque;
+                }
+                else
+                {
+                    Console.WriteLine("você não tem saldo suficiente para este saque!");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("senha errada!");
+            }
+            Console.ReadKey();
+        }
+        public void VerSaldo(string senha)
+        {
+            if (senha == this.senha)
+            {
+                Console.WriteLine("seu saldo é de:\t" + saldo);
+            }
+            else
+            {
+                Console.WriteLine("senha errada!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projetos em C#/Cofre/exercicioDia12do03/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioDia12do03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cofre cofre = new Cofre();
+            int escolha = 0;
+            do
+            {
+                Console.WriteLine("1 - Abrir cofre:");
+                Console.WriteLine("2 - fechar cofre");
+                Console.WriteLine("3 - depositar:");
+                Console.WriteLine("4 - sacar");
+                Console.WriteLine("5 - verificar saldo:");
+                Console.WriteLine("6 - sair");
+                escolha = int.Parse(Console.ReadLine());
+                if (escolha == 1)
+                {
+                    Console.WriteLine("digite a senha");
+                    string senha = Console.ReadLine(); 
+                    cofre.abrir(senha);
+                }
+                if (escolha == 2)
+                {
+                    Console.WriteLine("digite sua senha!");
+                    string senha = Console.ReadLine();
+                    cofre.fechar(senha);
+                }
+                if (escolha == 3)
+                {
+                    Console.WriteLine("digite sua senha para depositar");
+                    string senha = Console.ReadLine();
+                    cofre.depositar(senha);
+                }
+                if (escolha == 4)
+                {
+                    Console.WriteLine("digite sua senha");
+                    string senha = Console.ReadLine();
+                    cofre.sacar(senha);
+                }
+                if (escolha == 5)
+                {
+                    Console.WriteLine("digite sua senha");
+                    string senha = Console.ReadLine();
+                    cofre.VerSaldo(senha);
+                }
+                Console.Clear();
+            }
+            while (escolha != 6);
+
+        }
+    }
+}
